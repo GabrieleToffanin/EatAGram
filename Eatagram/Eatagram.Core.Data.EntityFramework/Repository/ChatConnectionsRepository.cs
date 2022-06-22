cb0a8fec1b24e2d2b3dac7cbd5b79a97bdaf117f
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2022-06-22 19:53:48 +0200

    Chat repository needs to be implemented

---
+++ Eatagram/Eatagram.Core.Data.EntityFramework/Repository/ChatConnectionsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Eatagram.Core.Data.EntityFramework.Contexts;
+using Eatagram.Core.Entities;
+using Eatagram.Core.Repository;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eatagram.Core.Data.EntityFramework.Repository
+{
+    public class ChatConnectionsRepository : IChatConnectionRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChatConnectionsRepository(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public ValueTask AddUserToExistingRoom(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<ApplicationUser>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Eatagram/Eatagram.Core/Entities/Chat/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eatagram.Core.Entities.Chat
+{
+    public class Connection
+    {
+        public string ConnectionID { get; set; }
+        public string UserAgent { get; set; }
+        public bool Connected { get; set; }
+    }
+}
