0d33d521dcdd248e2a9e655474ed50a98ef9708e
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2022-07-18 22:54:00 +0200

    Some kibana Stuff

---
+++ Backend/Eatagram/Eatagram.Core.Data.EntityFramework/Migrations/20220717174955_Initial Migration.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eatagram.Core.Data.EntityFramework.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    UpVoted = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientRecipe",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<int>(type: "int", nullable: false),
+                    RecipesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientRecipe", x => new { x.IngredientsId, x.RecipesId });
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Ingredients_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Recipes_RecipesId",
+                        column: x => x.RecipesId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_RecipeId",
+                table: "Comments",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientRecipe_RecipesId",
+                table: "IngredientRecipe",
+                column: "RecipesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_Name",
+                table: "Ingredients",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "IngredientRecipe");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -24,3 +24,9 @@ This Api purpouse is to enable mostly `CRUD` operations persistent in the databa
 
 - `YML Pipeline` :
   > The pipeline [Azure-WebApps-Core-Deployment](.github/workflows/azure-webapps-dotnet-core.yml) starts on `main` branch when push, builds and creates an artifacts of the ZIP compressed application and then publish it on the Azure WebService
+
+- `ElasticSearch and Kibana Experimenting` :
+  >Acutally did some reserch about kibana and elastic search working togheter. Fact is i have inserted a Tracer into the Eatagram API and let the logs into Kibana. I filtered Them using the following REGEX ```
+  (?<timestamp>[\d+\-\:\.].{23})\+\d+\:\d+\s(?<LogLevel>\[+[\w+\]]+)\s\((?<RunningOn>[\w+]+)\|\)\s\(\w+\)\s(?<HttpMethod>\[+[\w+\]]+)\s(?<Endpoint>[\w+\/]+)\s\w+\s+\w+\s*\'(?<EndpointParameters>[\{\D+\W+\d+\}\-*\'*].*)\'\s*\(*\w+\:*(?<AuthenticatedUser>[\w+\s*]+)\)\s*\-*\s*\w+\:*\s*\-*\s*\w*\:*\s*(?<Duration>[\d+\.\s\w*+]+)```.
+  There is a stupid but working graph about some api Stats
+  ![KibanaGraph](https://C:\Users\Gabri\Desktop\EatAGram\screenshots\KibanaGraphs.png)
